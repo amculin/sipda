139b14f4d25628e2eb45a0aa946b6d19d6289fdf
Fahmi Auliya <amixcustomlinux@gmail.com>
2024-10-01 11:07:34 +0700

    [DEV] Admin Dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,9 +1,30 @@
-<?php
-/** @var yii\web\View $this */
-?>
-<h1>dashboard/index</h1>
-
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<div class="page-wrapper" data-menu-active="Beranda">
+    <div class="container-xl">
+        
+        <div class="page-header d-print-none">
+            <div class="row g-2 align-items-center">
+                <div class="col">
+                    <div class="page-pretitle mb-2">
+                        <ol class="breadcrumb" aria-label="breadcrumbs">
+                            <li class="breadcrumb-item active"><a href="index.php">Beranda</a></li>
+                        </ol>
+                    </div>
+                    <h2 class="page-title">Beranda</h2>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="alert mb-2">
+                <h3>
+                    Halo, selamat datang <strong><?= Yii::$app->user->identity->nama; ?></strong> di aplikasi pemasaran dan selamat bekerja.
+                </h3>
+                <p><i>
+                    "Fokuskan tujuan yang ingin didapat, jangan biarkan faktor lain menghalangi tujuan Anda."
+                </i></p>
+            </div>
+        </div>
+    </div>
+</div>
